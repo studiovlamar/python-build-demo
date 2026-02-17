ba4c6c55f7953385057b130b79f1150c9de72f9c
Marian <marian@example.com>
2026-02-17 12:30:38 +0200

    Stergem test care pica intentionat!

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,7 +4,4 @@ def test_calculate_total():
     assert calculate_total([10, 15, 25]) == 50
     assert calculate_total([]) == 0
 
-# test care pica intentionat !
-def test_calculate_total_fail():
-    prices = [10, 20, 30]
-    assert calculate_total(prices) == 999  
+
