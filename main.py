26dc95da128b52e20b43e940871e2db85b09ee20
studiovlamar <d.marian.valentin@gmail.com>
2026-02-17 20:03:06 +0200

    Change currency output from RON to EURO

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from utils import calculate_total
 if __name__ == "__main__":
     prices = [10, 15, 25]
     total = calculate_total(prices)
-    print(f"Totalul este: {total} RON")
+    print(f"Totalul este: {total} EURO")
